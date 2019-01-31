701cd907548b8c79a620b1590fd0347952cd1934
PBI\NEX8ARJ <vinay.damle@pb.com>
2019-01-31 09:55:58 +0530

    Swagger config comments

--- src/main/java/com/cs/orderbookmanagement/controllers/StatsticsController.java
+++ src/main/java/com/cs/orderbookmanagement/controllers/StatsticsController.java
@@ -1,11 +1,12 @@
 package com.cs.orderbookmanagement.controllers;
 
-import com.cs.orderbookmanagement.entities.OrderDetail;
 import com.cs.orderbookmanagement.models.OrderResponse;
 import com.cs.orderbookmanagement.models.OrderState;
 import com.cs.orderbookmanagement.models.OrderStatstics;
 import com.cs.orderbookmanagement.services.StatsticsService;
 import com.cs.orderbookmanagement.utils.JSONMapper;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,6 +26,8 @@ public class StatsticsController {
     private StatsticsService orderDetailsStatsticsService;
 
     @GetMapping(value = "/orders", produces = {"application/json"}, consumes = {"application/json"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully gets the all order details including"),
+            @ApiResponse(code = 500, message = "Internal server error.")})
     public ResponseEntity<List<OrderResponse>> getAllOrders() {
         HttpStatus getAllOrdersHttpStatus = HttpStatus.OK;
         List<OrderResponse> orderDetailsList = orderDetailsStatsticsService.getAllOrders();
@@ -32,6 +35,9 @@ public class StatsticsController {
     }
 
     @GetMapping(produces = {"application/json"}, consumes = {"application/json"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully gets the all order details including " +
+            "total number of orders, valid and invalid order count"),
+            @ApiResponse(code = 500, message = "Internal server error.")})
     public ResponseEntity<OrderStatstics> getOrderStatstics(@RequestParam(value = "fetchValidInvalidRecords",
             required = false, defaultValue = "false") boolean fetchValidInvalidRecords) {
         HttpStatus getOrderStatsticsHttpStatus = HttpStatus.OK;
@@ -43,6 +49,8 @@ public class StatsticsController {
     }
 
     @GetMapping(value = "/{instrumentId}/orders", produces = {"application/json"}, consumes = {"application/json"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully gets the order details by given instrumentId."),
+            @ApiResponse(code = 500, message = "Internal server error.")})
     public ResponseEntity<List<OrderResponse>> getOrderDetailsByInstId(@PathVariable Long instrumentId) {
         HttpStatus orderDetailsByInstIdHttpStatus = HttpStatus.OK;
         List<OrderResponse> orderDetailsList = orderDetailsStatsticsService.getOrderDetailsByInstId(instrumentId);
@@ -53,6 +61,8 @@ public class StatsticsController {
     }
 
     @GetMapping(value = "/order/{orderId}", produces = {"application/json"}, consumes = {"application/json"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully gets the order details by given orderId."),
+            @ApiResponse(code = 500, message = "Internal server error.")})
     public ResponseEntity<OrderResponse> getOrderDetailsByOrderId(@PathVariable Long orderId) {
         HttpStatus orderDetailsByOrderIdHttpStatus = HttpStatus.OK;
         OrderResponse orderDetails = orderDetailsStatsticsService.getOrderDetailsByOrderId(orderId);
@@ -63,6 +73,8 @@ public class StatsticsController {
     }
 
     @GetMapping(value = "/order/{orderId}/orderState", produces = {"application/json"}, consumes = {"application/json"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully gets the order state of an order by given orderId."),
+            @ApiResponse(code = 500, message = "Internal server error.")})
     public ResponseEntity<OrderState> getOrderStateByOrderId(@PathVariable Long orderId) {
         HttpStatus orderStateByOrderIdHttpStatus = HttpStatus.OK;
         OrderState orderState = orderDetailsStatsticsService.getOrderStateByOrderId(orderId);
--- src/test/java/com/cs/orderbookmanagement/controllers/OrderBookControllerTests.java
+++ src/test/java/com/cs/orderbookmanagement/controllers/OrderBookControllerTests.java
@@ -25,7 +25,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
